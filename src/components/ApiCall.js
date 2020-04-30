e780fa40ac3be5225cc1bb6eda846e1b333703a5
Feltesse <er.feltesse@gmail.com>
2020-04-30 02:09:25 +0200

    résolution appel API

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -1,52 +1,36 @@
-mport React from "react";
-export default class Api extends React.Component {
+import React from "react";
+
+export default class ApiCall extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoaded: false,
-      error: null,
-      item: [],
+      loading: true,
+      webcam: null,
     };
-    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
-  componentDidMount() {
-    fetch(
-      "https://api.windy.com/api/webcams/v2/list/webcam=1583783403,1179853135,1384974006,1249044918,1473374057,1171033401,1284198909,1512053133,1206745451,1549489452?show=webcams:image,location&KEY=QuYS1wjNqXNc6LqfDlvVDbSvFGBM7uId"
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log(result["result:webcams"]);
-          this.setState({
-            isLoaded: true,
-            item: result["result:webcams"],
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
+  async componentDidMount() {
+    const url =
+      "https://api.windy.com/api/webcams/v2/list/webcam=1583783403,1179853135,1384974006,1249044918,1473374057,1171033401,1284198909,1512053133,1206745451,1549489452?show=webcams:image,location,player&key=QuYS1wjNqXNc6LqfDlvVDbSvFGBM7uId";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ webcam: data.result.webcams[0], loading: false });
   }
 
   render() {
-    const { item, isLoaded, error } = this.state;
-        if (error) {
-            return <div>Erreur : {error.message}</div>;
-          } else if (!isLoaded) {
-            return <div>Chargement...</div>;
-          } else {
-            return (
-              <div>
-                {item.map(item => (
-                  <div key={item.title}>
-                    {item.image.current.preview} {item.location.country}
-                  </div>
-                ))}
-              </div>
-            );
-          }
-        }}
+    return (
+      <div>
+        {this.state.loading || !this.state.webcam ? (
+          <div>...Je me lance !!!</div>
+        ) : (
+          <div>
+            <div>{this.state.webcam.title}</div>
+            <div>{this.state.webcam.location.country}</div>
+            <img src={this.state.webcam.image.current.preview} alt={this.state.webcam.title} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
